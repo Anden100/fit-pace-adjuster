a096386287588b121a7c3dfb1586e705d295635e
Anden100 <anden100@hotmail.com>
2026-02-17 23:26:21 +0100

    Add PWA support with offline functionality

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: '/fit-pace-adjuster/',
@@ -11,5 +12,51 @@ export default defineConfig({
     },
     outDir: 'dist',
     assetsDir: 'assets'
-  }
+  },
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'FIT Pace Adjuster',
+        short_name: 'FIT Adjuster',
+        description: 'Adjust paces in your Garmin FIT files for treadmill running activities',
+        theme_color: '#3498db',
+        background_color: '#f5f7fa',
+        display: 'standalone',
+        scope: '/fit-pace-adjuster/',
+        start_url: '/fit-pace-adjuster/',
+        icons: [
+          {
+            src: '/fit-pace-adjuster/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/fit-pace-adjuster/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'cdn-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365
+              }
+            }
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
+  ]
 })
